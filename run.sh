a1f0fb2cf72e742b9613e86c30cc48d48f7fbf11
cmauceri <126581901+cmauceri@users.noreply.github.com>
2024-04-16 13:32:57 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,42 +1,30 @@
 #!/bin/bash
-
-# if you need cvmfs or a given architecture
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-cd CMSSW_10_6_30_patch1/src/;
-cmsenv;
-source /cvmfs/cms.cern.ch/common/crab-setup.sh;
-cd ~/condor_start
-export SCRAM_ARCH=slc7_amd64_gcc820
-export HOME=.
+export SCRAM_ARCH=el9_amd64_gcc12
+cmsrel CMSSW_13_3_0
+cd CMSSW_13_3_0/src/
+cmsenv
+cd ../..
+source /cvmfs/cms.cern.ch/common/crab-setup.sh
+
+# replace this with x509up_u<your mit user id>
 export X509_USER_PROXY=x509up_u257692
-use_x509userproxy=true
+
 echo "hostname"
 hostname
 
 echo "filename"
 echo $2
-voms-proxy-info
-input_file="TEST_filelist.txt"
-
-# Check if the input file exists
-if [ ! -f "$input_file" ]; then
-    echo "File $input_file does not exist."
-    exit 1
-fi
-
-# Loop through each line in the input file
-while IFS= read -r filename; do
-    # Check if the filename is not empty
-    if [ -n "$filename" ]; then
-        xrdcp root://cmsxrootd.fnal.gov//"$filename" local.root
-        # Run the Python script with the current filename as input
-        python2 dat_muon_efficiency.py --input "$filename"
-        rm local.root
-    fi
-    done < "$input_file"
 
 
+xrdcp root://cmsxrootd.fnal.gov//"$2" "local.root"
+
+echo "ls"
+ls
+python3 dat_muon_efficiency.py --era "2016"
+filename=$(basename "$2")
 
 
 echo "----- transferring output to scratch :"
 echo " ------ THE END (everyone dies !) ----- "
+
