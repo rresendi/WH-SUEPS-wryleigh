2c31e5cf4b2f345e3e42e6ccf8bb326cdc1ad87f
cmauceri <126581901+cmauceri@users.noreply.github.com>
2024-04-18 10:47:25 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-export SCRAM_ARCH=el9_amd64_gcc12
-cmsrel CMSSW_13_3_0
-cd CMSSW_13_3_0/src/
-cmsenv
-cd ../..
-source /cvmfs/cms.cern.ch/common/crab-setup.sh
-
-# replace this with x509up_u<your mit user id>
 export X509_USER_PROXY=x509up_u257692
+
 
 echo "hostname"
 hostname
@@ -16,15 +9,46 @@ hostname
 echo "filename"
 echo $2
 
+cat /etc/os-release
+export SYSTEM_RELEASE=`cat /etc/redhat-release`
+if [[ $SYSTEM_RELEASE == *"release 7"* ]]; then
+    scram_arch=slc7_amd64_gcc10
+    cmssw_version=CMSSW_12_4_8
+elif [[ $SYSTEM_RELEASE == *"release 8"* ]]; then
+    scram_arch=el8_amd64_gcc10
+    cmssw_version=CMSSW_12_4_8
+
+elif [[ $SYSTEM_RELEASE == *"release 9"* ]]; then
+    scram_arch=el9_amd64_gcc11
+    cmssw_version=CMSSW_13_3_3
+else
+    echo "No default scram_arch for current OS!"
+    return 1;
+fi
+echo "scram arch:"
+echo $scram_arch
+echo "cmssw version:"
+echo $cmssw_version
+
+export SCRAM_ARCH=$scram_arch
+cmsrel $cmssw_version
+cd $cmssw_version/src
+cmsenv
+cd ../..
+source /cvmfs/cms.cern.ch/common/crab-setup.sh
 
 xrdcp root://cmsxrootd.fnal.gov//"$2" "local.root"
 
 echo "ls"
+
 ls
+
 python3 dat_muon_efficiency.py --era "2016"
 filename=$(basename "$2")
 
 
+
 echo "----- transferring output to scratch :"
-echo " ------ THE END (everyone dies !) ----- "
+
+
 
