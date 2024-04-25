7d3b33976c2443c198940dcc3740c8a4a94a7d60
cmauceri <126581901+cmauceri@users.noreply.github.com>
2024-04-25 14:37:42 -0400

    Update dat_muon_efficiency.py
    This is without any additional phase space cuts

--- dat_muon_efficiency.py
+++ dat_muon_efficiency.py
@@ -14,7 +14,7 @@ parser.add_argument("--era", help="data era", type=str)
 args = vars(parser.parse_args())
 input_file = "local.root"
 year=args["era"]
-output_file= "out.root"
+output_file= "ak4out.root"
 # Gets relevant variables from file
 def Events(f):
     evs=f['Events'].arrays(['HLT_IsoMu27',
@@ -34,32 +34,25 @@ def Events(f):
                 'Flag_BadPFMuonFilter',
                 'Flag_BadPFMuonDzFilter',
                 'Flag_eeBadScFilter',
-                'Flag_ecalBadCalibFilter'])
+                'Flag_ecalBadCalibFilter',
+                'nJet'])
 
     return evs
 
 # Defines binning and histograms
 mu_bin_edges=array('d',[0,2,4,6,8,10,12,
-                         14,16,18,20,22,
-                                                 24,26,28,30,32,
+                                                 14,16,18,20,22,
+                         24,26,28,30,32,
                          34,36,38,40,50,
                          60,70,80,90,100,
                          120,140,160,180,200])
 
-# Split into three regions of eta
-eta1_mu_num=ROOT.TH1D("eta_1_num","Muon events passing trigger eta region 1",len(mu_bin_edges)-1,mu_bin_edges)
-eta1_mu_denom=ROOT.TH1D("eta1_denom","Total Muon Events eta region 1",len(mu_bin_edges)-1,mu_bin_edges)
-eta2_mu_num=ROOT.TH1D("eta2_num","Muon events passing trigger eta region 2",len(mu_bin_edges)-1,mu_bin_edges)
-eta2_mu_denom=ROOT.TH1D("eta2_denom","Total Muon Events eta region 2",len(mu_bin_edges)-1,mu_bin_edges)
-eta3_mu_num=ROOT.TH1D("eta3_num","Muon events passing trigger eta region 3",len(mu_bin_edges)-1,mu_bin_edges)
-eta3_mu_denom=ROOT.TH1D("eta3_denom","Total Muon Events eta region 3",len(mu_bin_edges)-1,mu_bin_edges)
-eta_hists=[[eta1_mu_num,eta1_mu_denom],[eta2_mu_num,eta2_mu_denom],[eta3_mu_num,eta3_mu_denom]]
 
 # Function for filling the histograms
 
-def muon_hists(events,etas,hists,year):
-    mu_num=hists[0]
-    mu_denom=hists[1]
+def muon_hists(events,etas,year,reg):
+    mu_num=ROOT.TH1D("eta_"+reg+"_num","Muon events passing trigger eta region "+reg,len(mu_bin_edges)-1,mu_bin_edges)
+    mu_denom=ROOT.TH1D("eta_"+reg+"_denom","Total Muon Events eta region "+reg,len(mu_bin_edges)-1,mu_bin_edges)
     eta_min=etas[0]
     eta_max=etas[1]
     # trigger
@@ -82,14 +75,13 @@ def muon_hists(events,etas,hists,year):
             &(events["Muon_mediumId"])
             & (events["Muon_pt"] > 10)
             & (np.abs(events["Muon_eta"]) < 2.4)
-                        & (np.abs(events["Muon_dz"]) < 0.1)
+            & (np.abs(events["Muon_dz"]) < 0.1)
             & (np.abs(events["Muon_dxy"]) < 0.02)
             & (events["Muon_pfRelIso03_chg"] < 0.25)
             & (events["Muon_pfRelIso03_all"] < 0.25)
         )
     else:
-        muon_quality_check = (
-            (events['Flag_goodVertices'])
+        muon_quality_check = ((events['Flag_goodVertices'])
             &(events['Flag_globalSuperTightHalo2016Filter'])
             &(events['Flag_HBHENoiseFilter'])
             &(events['Flag_HBHENoiseIsoFilter'])
@@ -104,6 +96,7 @@ def muon_hists(events,etas,hists,year):
             & (np.abs(events["Muon_dxy"]) < 0.02)
             & (events["Muon_pfRelIso03_chg"] < 0.25)
             & (events["Muon_pfRelIso03_all"] < 0.25)
+            & (events["nJet"]==1)
         )
     # cut on eta
     eta_split=(
@@ -112,33 +105,27 @@ def muon_hists(events,etas,hists,year):
     )
     # Select based on trigger
     mu=events["Muon_pt"]
+    tr_evs=mu[muon_quality_check & eta_split & triggerSingleMuon]
     evs=mu[muon_quality_check & eta_split]
-    tr_evs=evs[triggerSingleMuon]
-
     #Fill histograms
-    for ev in evs:
-        for entry in ev:
-            mu_denom.Fill(entry)
     for ev in tr_evs:
         for entry in ev:
             mu_num.Fill(entry)
-            
-    return 0
+    for ev in evs:
+        for entry in ev:
+            mu_denom.Fill(entry)
+
+    return mu_num,mu_denom
 
 with uproot.open(input_file) as f:
     evs=Events(f)
-    eta_split=[[0.0,0.9],[0.9,2.1],[2.1,2.4]]
-    for (etas,hists) in zip(eta_split,eta_hists):
-        muon_hists(evs,etas,hists,year)
-print("debugging")
-print("num1")
-print(type(eta1_mu_num))
-print("denom 1")
-print(type(eta1_mu_denom))
-c1 = ROOT.TCanvas ("canvas","",800,600)
-eta1_mu_denom.Draw()
-c1.Update()
-c1.SaveAs("Efficiency.png")
+    eta1_mu_num=(muon_hists(evs,[0.0,0.9],year,"1"))[0]
+    eta1_mu_denom=(muon_hists(evs,[0.0,0.9],year,"1"))[1]
+    eta2_mu_num=(muon_hists(evs,[0.9,2.1],year,"2"))[0]
+    eta2_mu_denom=(muon_hists(evs,[0.9,2.1],year,"2"))[1]
+    eta3_mu_num=(muon_hists(evs,[2.1,2.4],year,"3"))[0]
+    eta3_mu_denom=(muon_hists(evs,[2.1,2.4],year,"3"))[1]
+
 # Saves overall efficiency
 root_file = ROOT.TFile(output_file,"RECREATE")
 root_file.cd()
@@ -152,4 +139,7 @@ eta3_mu_denom.Write()
 root_file.Close()
 
 print("sample complete")
-                        
+
+
+
+
