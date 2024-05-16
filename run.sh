e009061ea8398cef5a3673c4d8a2f323c40d4ad6
rresendi <153397132+rresendi@users.noreply.github.com>
2024-05-16 13:43:58 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                                                                                                           
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 export X509_USER_PROXY=x509up_u257692
+voms-proxy-info
+
+
+
 
 
 echo "hostname"
@@ -8,6 +12,9 @@ hostname
 
 echo "filename"
 echo $2
+
+xrdcp root://cmsxrootd.fnal.gov//$2 local.root
+
 
 cat /etc/os-release
 export SYSTEM_RELEASE=`cat /etc/redhat-release`
@@ -35,20 +42,21 @@ cmsrel $cmssw_version
 cd $cmssw_version/src
 cmsenv
 cd ../..
-source /cvmfs/cms.cern.ch/common/crab-setup.sh
+#source /cvmfs/cms.cern.ch/common/crab-setup.sh                                                                                                                                                                       
 
-xrdcp root://cmsxrootd.fnal.gov//"$2" "local.root"
 
 echo "ls"
 
 ls
 
-python3 dat_muon_efficiency.py --era "2016"
-filename=$(basename "$2")
+python3 dat_muon_efficiency.py  --era "2016"
 
+filename=$(basename "$2")
+echo "basename filename"
+echo $filename
+xrdcp out.root root://submit50.mit.edu//cmauceri/dat_mu_effs_2016/$filename
 
 
 echo "----- transferring output to scratch :"
-
-
+echo " ------ THE END (everyone dies !) ----- "
 
